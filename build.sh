ffa8b5a7e9ca6bf7b9ce5a6dcf6e414a916fa554
Life <74325525+Life683@users.noreply.github.com>
2026-07-06 01:55:28 -0400

    Remove CLI building

--- build.sh
+++ build.sh
@@ -6,17 +6,13 @@ dotnet publish -c Release --runtime osx-x64 --self-contained
 #dotnet publish -c Release --runtime osx-arm64 --self-contained
 
 mkdir BuildOutput
-cp "./BeatmapExporterCLI/bin/Release/net10.0/win-x64/publish/BeatmapExporterCLI.exe" "BuildOutput/BeatmapExporterCLI.exe"
-cp "./BeatmapExporterCLI/bin/Release/net10.0/osx-x64/publish/BeatmapExporterCLI" "BuildOutput/mac-BeatmapExporterCLI"
-#cp "./BeatmapExporterCLI/bin/Release/net10.0/osx-arm64/publish/BeatmapExporterCLI" "BuildOutput/mac-arm64-BeatmapExporterCLI"
-cp "./BeatmapExporterCLI/bin/Release/net10.0/linux-x64/publish/BeatmapExporterCLI" "BuildOutput/linux-BeatmapExporterCLI"
-cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/win-x64/publish/BeatmapExporterGUI.Desktop.exe" "BuildOutput/BeatmapExporter.exe"
-cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/osx-x64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/x86-BeatmapExporter.app"
-#cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/osx-arm64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/arm64-BeatmapExporter.app"
-cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/linux-x64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/linux-BeatmapExporter"
+cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/win-x64/publish/BeatmapExporterGUI.Desktop.exe" "BuildOutput/BeatmapCleaner.exe"
+cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/osx-x64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/x86-BeatmapCleaner.app"
+#cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/osx-arm64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/arm64-BeatmapCleaner.app"
+cp "./BeatmapExporterGUI.Desktop/bin/Release/net10.0/linux-x64/publish/BeatmapExporterGUI.Desktop" "BuildOutput/linux-BeatmapCleaner"
 
 cd BuildOutput
-tar -avcf "mac-BeatmapExporter.zip" "x86-BeatmapExporter.app" --transform 's/x86-//'
-#tar -avcf "mac-arm64-BeatmapExporter.zip" "arm64-BeatmapExporter.app" --transform 's/arm64-//'
-rm x86-BeatmapExporter.app
-#rm arm64-BeatmapExporter.app
+tar -avcf "mac-BeatmapCleaner.zip" "x86-BeatmapCleaner.app" --transform 's/x86-//'
+#tar -avcf "mac-arm64-BeatmapCleaner.zip" "arm64-BeatmapCleaner.app" --transform 's/arm64-//'
+rm x86-BeatmapCleaner.app
+#rm arm64-BeatmapCleaner.app
